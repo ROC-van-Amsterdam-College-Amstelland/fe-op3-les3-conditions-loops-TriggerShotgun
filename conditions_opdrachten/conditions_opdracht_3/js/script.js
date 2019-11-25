25cd97ff48a9529c629cab7a2785e7d40f61caa3
Roan Lindeboom <roa_lindeboom@hotmail.com>
2019-11-25 15:02:25 +0100

    3 done

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+function check(){
+    var saldo= document.getElementById("saldo").value;
+
+    if(saldo==500){
+        document.getElementById("tekst").innerHTML = "salaris gestort" ;
+    }else if(saldo>500){
+        document.getElementById("tekst").innerHTML = "salaris + bonus gestort";
+    }else {
+        document.getElementById("tekst").innerHTML = "niks gestort";
+    }
+    
+
+}
