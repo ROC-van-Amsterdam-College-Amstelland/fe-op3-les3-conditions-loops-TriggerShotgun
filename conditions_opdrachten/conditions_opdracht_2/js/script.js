6b496860bb54379871f15b7fbfaf97ea3fd786fd
Roan Lindeboom <roa_lindeboom@hotmail.com>
2019-11-25 14:55:05 +0100

    2 done

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+function check(){
+    var saldo= document.getElementById("saldo").value;
+
+    if(saldo>=25){
+        document.getElementById("tekst").innerHTML = "Je hebt voldoende saldo om € 25 op te nemen." ;
+    }else{
+        document.getElementById("tekst").innerHTML = "Sorry, je hebt te weinig saldo!";
+    }
+    
+
+}
