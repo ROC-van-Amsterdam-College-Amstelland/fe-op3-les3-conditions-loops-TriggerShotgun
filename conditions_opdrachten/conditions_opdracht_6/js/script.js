c4a15608bb682b5e19078e4fa785befc08ce8d8d
Roan Lindeboom <roa_lindeboom@hotmail.com>
2019-11-26 13:22:32 +0100

    5+6

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,15 @@
-//javascript code
+function check() {
+
+    var myNumber1 = parseInt(document.getElementById("myNumber1").value);
+    var myNumber2 = parseInt(document.getElementById("myNumber2").value);
+    var myOperator = document.getElementById("myOperator").value;
+
+    if (myOperator == "+") {
+        document.getElementById("tekst").innerHTML = myNumber1 + myNumber2;
+    } else if (myOperator == "-") {
+        document.getElementById("tekst").innerHTML = myNumber1 - myNumber2;
+    } else if (myOperator == "*"){
+        document.getElementById("tekst").innerHTML = myNumber1 * myNumber2;
+    }
+
+}
