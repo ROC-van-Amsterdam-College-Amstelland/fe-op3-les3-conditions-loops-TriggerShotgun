d40cf09b91b80cf29a27765a4f3a5480580bd0fb
Roan Lindeboom <roa_lindeboom@hotmail.com>
2019-12-02 14:46:31 +0100

    loops 3 +4 af

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,7 @@
 //javascript code
+var tekst="";
+for(i = 0; i <51 ; i++) {
+  tekst += i + "<br>";
+  console.log(i);
+}
+document.getElementById("tekst").innerHTML = tekst;//javascript code
--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,1 +1,7 @@
 //javascript code
+var tekst="";
+for(i = 0; i <1001 ; i+=10) {
+  tekst += i + "<br>";
+  console.log(i);
+}
+document.getElementById("tekst").innerHTML = tekst;
